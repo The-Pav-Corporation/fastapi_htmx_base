5e29584d23e64e6d3f51a8601f376e25bb8e7d16
Alexandru Pavilcu <alexandrupavilcu@gmail.com>
2022-02-07 20:29:07 +0200

    fix secret leak

--- project/app/settings.py
+++ project/app/settings.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 from fastapi.templating import Jinja2Templates
 from passlib.context import CryptContext
+from ...secrets import ULTRA_SECRET_KEY
 import jinja_partials
 
 BASE_DIR = Path(__file__).resolve().parent
@@ -13,7 +14,7 @@ jinja_partials.register_starlette_extensions(templates)
 ### Security ###
 # to get a string like this run:
 # openssl rand -hex 32
-SECRET_KEY = "574a7f0129e7ff33fdebef3a29d0e5d9d8817059f9b98721c742624f94079afd"
+SECRET_KEY = ULTRA_SECRET_KEY
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
